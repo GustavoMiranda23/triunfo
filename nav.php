6a861dc5c652795135fb5ca47c5bb0b24e1c0583
Gustavo Miranda <onemoregustavo@gmail.com>
2022-11-22 16:41:14 -0300

    inicial

---
+++ nav.php
@@ -0,0 +1,57 @@
+<div class="has-background-black">
+    <td>
+        <p class="has-text-centered has-text-white">
+            Novos Produtos!
+        </p>
+    </td>
+</div>
+<nav>
+    <div class="columns">
+        <div class="column"><br></div>
+        <div class="column is-three-fifths">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-one-quarter">
+                        <figure class="image is-3by1 is-covered"></figure>
+                        <a href="index.html"><img src="imgs/triunfologo.jpg"></a>
+                    </div>
+                    <div class="column">
+                        <br><br>
+
+                        <div class="field">
+                            <p class="control is-expanded has-icons-right ">
+                                <input class="input" type="search" placeholder="Buscar..." />
+                                <span class="icon is-small is-right"><i class="fas fa-search"></i></span>
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <h3 class="px-4 mx-6 has-text-left has-text-whites"><br><br><br> <a href="index.html" class="has-text-centered has-text-black">Entre ou cadastre-se </a></h3>
+                </div>
+                <div class="columns is-flex-direction-row">
+                    <div class="c p-2 bd-highlight
+                has-background"> <a class="has-text-black" href="camisas.html">CAMISAS</a>
+                    </div>
+                    <div class="c p-2 bd-highlight
+                has-background"> <a class="has-text-black" href="acessorios.html"> ACESSÓRIOS </a>
+                    </div>
+                    <div class="c p-2 bd-highlight
+                has-background"> <a class="has-text-black" href="calcas.html">CALÇAS</a>
+                    </div>
+                    <div class="c p-2 bd-highlight
+                has-background"> <a class="has-text-black" href="moletons.html">MOLETONS</a>
+                    </div>
+                    <div class="c p-2 bd-highlight has-background"> <a class="has-text-black" href="oculos.html">ÓCULOS</a> </div>
+                    <div class="c p-2 bd-highlight has-background"> <a class="has-text-black" href="bermudas.html">BERMUDAS</a> </div>
+                    <div class="c p-2 bd-highlight has-background"> <a class="has-text-black" href="calcados.html">CALÇADOS</a> </div>
+                    <div class="c p-2 bd-highlight has-background"> <a class="has-text-black" href="intimos.html">ÍNTIMOS</a> </div>
+                    <div class="c p-2 bd-highlight has-background"> <a class="has-text-black" href="estilos.html">ESTILOS</a> </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="column"><br></div>
+    </div>
+    <div class="d"><br></div>
+</nav>
